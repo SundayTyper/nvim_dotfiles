286f4a761e521609bdad03062c86abc06c8b4b17
e-whitehead-WM <edward.whitehead@wmfts.com>
2025-11-12 14:06:29 +0000

    set tabs to 4 and show whitespace

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,8 +7,8 @@ vim.opt.textwidth = 80
 vim.opt.cursorline = true
 
 -- Tabs and spaces
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.softtabstop = 2
 
@@ -28,5 +28,10 @@ vim.opt.smartcase = true  -- Case-sensitive search if there are uppercase charac
 vim.opt.backspace = "indent,eol,start" -- Allow backspacing over indent, EOL, and start of line
 vim.opt.showcmd = true                 -- Show typed commands
 vim.opt.wildmenu = true                -- Enable command-line completion
-vim.opt.winborder = 'rounded'
+vim.opt.winborder = 'rounded'          -- rounded borders for popus
 vim.opt.clipboard = 'unnamedplus'      -- connect to system clipboard
+
+-- show invisible characters
+vim.opt.list = true
+vim.opt.listchars = { tab = '»·', trail = '•', lead = '◦' }
+
