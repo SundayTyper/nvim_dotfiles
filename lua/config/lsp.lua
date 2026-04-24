f794bfc14e1cfe43b261833f775fcae9dd1bbd43
e-whitehead-WM <edward.whitehead@wmfts.com>
2026-04-24 13:54:02 +0100

    only show hints in insert mode

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -9,9 +9,23 @@ vim.api.nvim_create_autocmd("LspAttach", {
       -- Set buffer-local options
       vim.opt_local.omnifunc = "v:lua.vim.lsp.omnifunc"
 
-      -- Enable inlay hints if supported (Neovim 0.10+)
+      -- Enable inlay hints only in insert mode (Neovim 0.10+)
       if vim.lsp.inlay_hint and client.server_capabilities.inlayHintProvider then
-        vim.lsp.inlay_hint.enable(true, { bufnr = ev.buf })
+        local group = vim.api.nvim_create_augroup("LspInlayHints" .. ev.buf, { clear = true })
+        vim.api.nvim_create_autocmd("InsertEnter", {
+          group = group,
+          buffer = ev.buf,
+          callback = function()
+            vim.lsp.inlay_hint.enable(true, { bufnr = ev.buf })
+          end,
+        })
+        vim.api.nvim_create_autocmd("InsertLeave", {
+          group = group,
+          buffer = ev.buf,
+          callback = function()
+            vim.lsp.inlay_hint.enable(false, { bufnr = ev.buf })
+          end,
+        })
       end
     end
   end,
